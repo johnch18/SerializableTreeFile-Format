71006e959b93f8304ac6efe41145604b032f2961
Charles Johnson <johnch18@isu.edu>
2024-01-17 19:01:34 -0700

    rewrite 1.0

---
+++ stf/core.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3.11
+
+"""
+SerializableTreeFile-Format
+core.py
+stf
+"""
+
+__all__: list[str] = []
+
+
+def main() -> None:
+    """
+    Main entrypoint
+    """
+
+
+if __name__ == "__main__":
+    main()
+else:
+    pass
